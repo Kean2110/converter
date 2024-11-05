78c7fe6bd97181d5a70570213f53354a52c05001
kiani <kiani@cip.ifi.lmu.de>
2024-11-06 00:25:18 +0100

    adding list of ignored input files

--- img_converter.py
+++ img_converter.py
@@ -7,9 +7,9 @@ from rich.progress import track
 input_dir = "input/"
 output_dir = "output/"
 output_format = ".jpg"
+excluded_files = [".gitkeep"]
 
-
-input_files = os.listdir(input_dir)
+input_files = [file for file in os.listdir(input_dir) if not file in excluded_files]
 for file in track(input_files):
     file_name, file_extension = os.path.splitext(file)
     img = os.path.join(input_dir, file)
